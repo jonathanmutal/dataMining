2ea364a4622c5a29f2669ab8961b208dfcd38b9e
jonathanmutal <jonathanmutal95@gmail.com>
2017-09-11 21:55:55 -0300

    second atempt for k-means clustering

--- clustering.py
+++ clustering.py
@@ -1,4 +1,10 @@
-class K_mean:
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+import random
+from collections import defaultdict
+
+class K_means:
     """
     This class is an implementation of K-means algorithm.
     """
@@ -16,18 +22,24 @@ class K_mean:
         data -- data to cluster (have to be a numpy array)
         """
         K = self.K
-        n = self.num_iter
+        n = self.n
+
+        # init the centroids randomly
+        centroids = {}
+        for k, idx in enumerate(np.random.choice(range(len(data)), K, replace=False)):
+            centroids[k] = data[idx, :]
 
         for i in range(n):
-            # init the centroids randomly
-            centroids = {}
-            for k, word_vec in enumerate(random.sample(data, K)):
-                centroids[k] = word_vec
-
             # run k-means. cluster aligment step
             # classifications
             classifications = defaultdict(list)
             for xi in data:
-                distances = [np.align.norm(xi - centroids[centroid]) for centroid in centroids]
+                distances = [np.linalg.norm(xi - centroids[centroid]) for centroid in centroids]
                 index_classification = distances.index(min(distances))
                 classifications[index_classification].append(xi)
+
+            # move centroids step.
+            for classif in classifications:
+                centroids[k] = np.average(classifications[classif], axis=0)
+
+k_means = K_means(2)
