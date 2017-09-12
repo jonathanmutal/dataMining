c5593eee176bbaa7e41932fbd9bf0699a49f60f0
jonathanmutal <jonathanmutal95@gmail.com>
2017-09-12 11:54:24 -0300

    some configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='dataMining',
+      version='0.1',
+      author='Jonathan David Mutal',
+      author_email='jdm0113@mail.famaf.unc.edu.ar',
+      url='http://www.cs.famaf.unc.edu.ar/~jdm0113/',
+      packages=find_packages(),
+    )
