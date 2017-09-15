0320aa753ce600ec98e667ce9f656f59522fbbb1
jonathanmutal <jonathanmutal95@gmail.com>
2017-09-15 00:01:31 -0300

    wrapper for word2vec

--- clustering/scripts/train_w2v.py
+++ clustering/scripts/train_w2v.py
@@ -1,49 +1,71 @@
 from nltk.data import LazyLoader
+from nltk.tokenize import word_tokenize
+
 import gensim.models.word2vec as w2v
 
 import re
 import codecs
+
+
+class W2V_wrapper:
+
+    def __init__(self, path='../../lavoztextodump.txt',
+                 word_tokenizer=word_tokenize,
+                 sent_tokenizer=LazyLoader('tokenizers/punkt/spanish.pickle')):
+        self.path = path
+        self.word_tokenizer = word_tokenizer
+        self.sent_tokenizer = sent_tokenizer
+
+    def __raw_data(self):
+        """
+        Should optimaze for big files
+        """
+        with codecs.open(self.path, 'r', 'utf-8') as f:
+            corpus = f.read()
+
+        return corpus
+
+    def __tokenize(self):
+        corpus = self.__raw_data()
+        corpus_sent = self.sent_tokenizer.tokenize(corpus)
+        clean_corpus = [regex.findall(re.sub('\d+', 'NUM', re.sub('&#\d+', '', sents))) for sents in corpus_sent]
+
+        return clean_corpus
+
+    def config(self):
+        """
+        Add some arguments to config
+        """
+        model = w2v.Word2Vec(
+            sg=1,
+            seed=1,
+            workers=4,
+            size=300,
+            min_count=3,
+            window=7,
+            sample=1e-3
+        )
+
+        self.model = model
+
+    def train(self):
+        clean_corpus = self.__tokenize()
+        self.model.build_vocab(clean_corpus)
+        print(self.model.iter)
+        self.model.train(clean_corpus, total_examples=self.model.corpus_count,
+                         epochs=self.model.iter)
+
+    def save(self):
+        self.model.save('model.w2v')
 
 pattern = r'''(?ix)    # set flag to allow verbose regexps
         (?:sr\.|sra\.|mr\.|mrs\.)
         | (?:[A-Z]\.)+        # abbreviations, e.g. U.S.A.
         | \w+(?:-\w+)*        # words with optional internal hyphens
         '''
-
-with codecs.open('../../lavoztextodump.txt', 'r', 'utf-8') as f:
-    corpus = f.read()
-
 regex = re.compile(pattern)
-sent_tokenizer = LazyLoader('tokenizers/punkt/spanish.pickle')
-corpus_sent = sent_tokenizer.tokenize(corpus)
-clean_corpus = [' '.join(regex.findall(re.sub('\d+', 'NUM', re.sub('&#\d+', '', sents)))) for sents in corpus_sent]
 
-model = w2v.Word2Vec(clean_corpus, sg=1, size=300, window=5, min_count=5, workers=4, compute_loss=True)
-model.save('model.w2v')
-# X = np.array([[1, 2],
-#               [1.5, 1.8],
-#               [5, 8],
-#               [8, 8],
-#               [1, 0.6],
-#               [9, 11],
-#               [1, 3],
-#               [8, 9],
-#               [0, 3],
-#               [5, 4],
-#               [6, 4]])
-
-# colors = 10 * ["g", "r", "c", "b", "k"]
-# clf = Kmeans(X, 3)
-# classifications, centroids = clf.get_best_fit()
-
-# for centroid in centroids:
-#     plt.scatter(centroids[centroid][0], centroids[centroid][1],
-#                 marker="o", color="k", linewidths=5)
-
-# for classification in classifications:
-#     color = colors[classification]
-#     for featureset in classifications[classification]:
-#         plt.scatter(featureset[0], featureset[1], marker="x",
-#                     color=color, linewidths=5)
-# plt.show()
-# to normalizate the data
+w2v_ = W2V_wrapper(word_tokenizer=regex)
+w2v_.config()
+w2v_.train()
+w2v_.save()
