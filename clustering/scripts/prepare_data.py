1bb220a2e0f94aaa0d13590c918c7677d973539f
jonathanmutal <jonathanmutal95@gmail.com>
2017-09-23 09:46:28 -0300

    first attempt to load data and save data

--- clustering/scripts/prepare_data.py
+++ clustering/scripts/prepare_data.py
@@ -1,5 +1,10 @@
-from vectorizer import W2V_wrapper, Featurize
-from preproccess import Normalization, TAG_norm
+from clustering.vectorizer import W2V_wrapper, Featurize
+from clustering.preproccess import Normalization, TAG_norm
+from clustering.clustering import Kmeans
+
+from sklearn.cluster import KMeans
+
+from collections import defaultdict
 
 import pickle
 
@@ -12,18 +17,36 @@ def w2v_train_data():
     w2v_.train()
     w2v_.save()
 
-def save_dict():
+def save_dict(filename):
     tagger = TAG_norm()
     tagg_data = tagger.tagger()
     featurize = Featurize(tagg_data)
     featurize.feat2dic()
-    featurize.class2pickle('test.pickle')
+    featurize.class2pickle(filename)
 
 def load_dict(filename):
     """
-    filename -- filename of pickle
+    filename -- pickle's file name
     """
     features = []
     with open(filename, 'rb') as f:
         features = pickle.load(f)
     return features
+
+if __name__ == '__main__':
+    save_dict('test.pickle')
+    v = load_dict('test.pickle')
+    matrix, words = v.dict2matrix(sparse=False)
+    # Km = KMeans().fit(matrix)
+
+    # clusters = defaultdict(set)
+    # for i, label in enumerate(Km.labels_):
+    #     clusters[label].add(words[i])
+
+    # for i, clus in enumerate(clusters):
+    #     print(i, clusters[clus])
+
+    clusters, centroids = Kmeans(matrix, 15).get_best_fit()
+    for cluster in clusters:
+        cl = set(map(lambda x: words[matrix.index(x)], clusters[cluster]))
+        print(cluster, cl)
