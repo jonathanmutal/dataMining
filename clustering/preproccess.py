e434f9d9f0cd3145d9ee64c37b8d2361b6532f42
jonathanmutal <jonathanmutal95@gmail.com>
2017-09-23 09:44:33 -0300

    adding option for chosing tagger. Method to remove digits

--- clustering/preproccess.py
+++ clustering/preproccess.py
@@ -1,6 +1,8 @@
 from nltk.data import LazyLoader
 from nltk.tag.stanford import StanfordPOSTagger
+from nltk.corpus import stopwords
 
+import spacy
 import codecs
 import re
 
@@ -13,7 +15,7 @@ pattern = r'''(?ix)    # set flag to allow verbose regexps
 regex = re.compile(pattern)
 
 class Normalization:
-    def __init__(self, path='../lavoztextodump.txt',
+    def __init__(self, path='/home/jonathan/dataMining/lavoztextodump.txt',
                  word_tokenizer=regex,
                  sent_tokenizer=LazyLoader('tokenizers/punkt/spanish.pickle')):
         self.path = path
@@ -22,26 +24,34 @@ class Normalization:
 
         # Should optimaze for big files
         with codecs.open(self.path, 'r', 'utf-8') as f:
-            corpus = f.read(1000)
+            corpus = f.read(6000)
 
         corpus_sent = self.__sent_tokenizer.tokenize(corpus)
         self.corpus_clean = [re.sub('&#\d+', '', sents) for sents in corpus_sent]
 
-    def digit_to_NUM(self, words):
-        return [(re.sub('\d+', 'NUM', word[0]), word[1:]) for word in words]
+    def dig2num(self, sents):
+        ready_sents = []
+        for sent in sents:
+            ready_sents.append([(re.sub('\d+', 'NUM', word[0]),) + word[1:] for word in sent if word[0].lower() not in stopwords.words('spanish')])
+        return ready_sents
 
     def tokenize(self):
-        clean_corpus = [self.__word_tokenizer.findall(sent) for sent in self.corpus_clean]
-
-        return clean_corpus
+        self.clean_corpus = [self.__word_tokenizer.findall(sent) for sent in self.corpus_clean]
 
 class TAG_norm(Normalization):
-    def __init_(self, path='~/dataMining/lavoztextodump.txt'):
+    def __init__(self, path='/home/jonathan/dataMining/lavoztextodump.txt', taggerUse='standford'):
         super().__init__(path=path)
 
+        self.taggerUse = taggerUse
+        self.tokenize()
+        if self.taggerUse != 'standford':
+            self.nlp = spacy.load('es_core_web_md')
+
     def tagger(self):
-        corpus_clean = self.tokenize()
-        tagger = StanfordPOSTagger('/home/jonathan/dataMining/stanford-postagger-full-2017-06-09/models/spanish-distsim.tagger',
-                                   '/home/jonathan/dataMining/stanford-postagger-full-2017-06-09/stanford-postagger-3.8.0.jar')
-        tagged_sents = tagger.tag_sents(corpus_clean)
-        return tagged_sents
+        if self.taggerUse == 'standford':
+            tagger = StanfordPOSTagger('/home/jonathan/dataMining/stanford-postagger-full-2017-06-09/models/spanish-distsim.tagger',
+                               '/home/jonathan/dataMining/stanford-postagger-full-2017-06-09/stanford-postagger-3.8.0.jar')
+            tagged_sents = tagger.tag_sents(self.clean_corpus)
+        else:
+            tagged_sents = self.nlp(self.clean_corpus, parse=False, entity=False)
+        return self.dig2num(tagged_sents)
